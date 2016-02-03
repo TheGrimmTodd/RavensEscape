72fe98c74f8452eb8d82c9e6ba0f06a9e78e32c0
DJ Todd <dtodd67@gmail.com>
2016-02-03 17:06:38 -0600

    got the endzone detection handled

--- Assets/Scrips/LevelSignalSender.cs
+++ Assets/Scrips/LevelSignalSender.cs
@@ -6,8 +6,15 @@ public class LevelSignalSender  {
 
 	private static volatile LevelSignalSender instance;
 	private static object syncRoot = new Object();
+	private System.Collections.ArrayList lightListeners;
+	private System.Collections.ArrayList endListeners;
+
 	
-	private LevelSignalSender() {}
+	private LevelSignalSender() {
+		lightListeners = new System.Collections.ArrayList ();
+		endListeners = new System.Collections.ArrayList ();
+		
+	}
 	
 	public static LevelSignalSender Instance
 	{
@@ -26,11 +33,74 @@ public class LevelSignalSender  {
 		}
 	}
 
+	/*Add Listeners*/
+	public void addLightTouchedListener(OnLightTouchedListener listener){
+		lock (syncRoot) 
+		{
+			lightListeners.Add (listener);
+		}
+	}
+	public void addEndZonetTouchedListener(OnEndZoneTouchedListener listener){
+		lock (syncRoot) 
+		{
+			endListeners.Add (listener);
+		}
+	}
+	/*Remove Listeners*/
+	public void removeLightTouchedListener(OnLightTouchedListener listener){
+		lock (syncRoot) 
+		{
+			lightListeners.Remove (listener);
+		}
+	}
+	public void removeEndZoneTouchedListener(OnEndZoneTouchedListener listener){
+		lock (syncRoot) 
+		{
+			endListeners.Remove (listener);
+		}
+	}
+
+
+
+
+
+
+
+
+	/*Signals*/
 	public void signalLightTouched()
 	{
 		lock (syncRoot) 
 		{
+			foreach( object obj in lightListeners)
+				if (obj.GetType().Equals(typeof(OnLightTouchedListener)))
+					((OnLightTouchedListener) obj).OnTouch();
+			//TODO: handle correctly
 			Application.LoadLevel (0);
 		}
 	}
+	public void signalEndZoneTouched()
+	{
+		lock (syncRoot) 
+		{
+			foreach( object obj in lightListeners)
+				if (obj.GetType().Equals(typeof(OnEndZoneTouchedListener)))
+					((OnEndZoneTouchedListener) obj).OnTouch();
+			//TODO: handle correctly
+			Application.LoadLevel (0);
+		}
+	}
+
+
+
+
+	/*Iterfaces*/
+	public interface OnLightTouchedListener{
+		void OnTouch ();
+	}
+	public interface OnEndZoneTouchedListener{
+		void OnTouch();
+	}
+
+
 }
