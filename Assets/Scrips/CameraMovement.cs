a5157725ddb3c78f63990d4a10ba882430e30473
DJ Todd <dtodd67@gmail.com>
2016-07-07 21:24:50 -0500

    quick implement of y camera movement... not great yet

--- Assets/Scrips/CameraMovement.cs
+++ Assets/Scrips/CameraMovement.cs
@@ -3,25 +3,52 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 
-	public float smooth = 0.5f;         
-	private Vector2 pvel;
-	public GameObject player;           
+	public const float smoothness = 0.5f;         
+	private Vector2 playerVelocity;
+	public GameObject target; 
+	public float freeMoveZoneY= 5f;
+	private bool moveY = false;
 
 	private Vector3 velocity = Vector3.zero;
 	// Use this for initialization
 	void Start () {
 		// Setting up the reference.
-//		player = GameObject.FindGameObjectWithTag("Player").transform;
-//		pvel = ((Rigidbody2D)GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody2D> ()).velocity;
-		pvel = player.GetComponent<Rigidbody2D> ().velocity;
+		playerVelocity = target.GetComponent<Rigidbody2D> ().velocity;
 	}
 
 	void FixedUpdate() {
-		if (player) {
+		if (target) {
+			followTargetX ();
+			followTargetY ();
+		}
+	}
+
+	private void followTargetX ()
+	{
+		var v = transform.position;
+		v.x = target.transform.position.x;
+		float velocity = playerVelocity.x == 0 ? smoothness : playerVelocity.x;
+		transform.position = Vector3.Lerp (transform.position, v, System.Math.Abs (velocity) * Time.deltaTime);
+	}
+
+	private void followTargetY ()
+	{
+
+		float distance = Mathf.Abs (target.transform.position.y - transform.position.y);
+		if (distance > freeMoveZoneY) 
+		{
+			moveY = true;
+		}
+		if (moveY) 
+		{
 			var v = transform.position;
-			v.x = player.transform.position.x;
-			float velocity = pvel.x == 0? smooth : pvel.x;
-			transform.position = Vector3.Lerp(transform.position, v, System.Math.Abs(velocity) * Time.deltaTime);
+			v.y = target.transform.position.y;
+			float velocity = playerVelocity.y == 0 ? smoothness : playerVelocity.y;
+			transform.position = Vector3.Lerp (transform.position, v, System.Math.Abs (velocity) * Time.deltaTime);
+			if (transform.position.y == target.transform.position.y)
+			{
+				moveY = false;
+			}
 		}
 	}
 }
