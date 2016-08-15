f0e033cf3109b52fbb7933a62f491ce93e2351f9
DJ Todd <dtodd67@gmail.com>
2016-08-14 20:27:45 -0500

    got wall jumping working

--- Assets/Scrips/RevenControler.cs
+++ Assets/Scrips/RevenControler.cs
@@ -1,18 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RevenControler : MonoBehaviour {
+public class RevenControler : MonoBehaviour
+{
 
 	
 	public static float jumpHeight = 8f;
-
 	private Vector2 FIRST = Vector2.up * jumpHeight;
 	private Vector2 SECOND = Vector2.up * jumpHeight * 0.6f;
-	private Vector2 WALL_RIGHT = new Vector2 (jumpHeight * 3f, jumpHeight * 0.2f);
-	private Vector2 WALL_LEFT = new Vector2 (jumpHeight * -3f, jumpHeight * 0.2f);
+	private Vector2 WALL_RIGHT = new Vector2 (jumpHeight * 0.5f, jumpHeight * 0.8f);
+	private Vector2 WALL_LEFT = new Vector2 (jumpHeight * -0.5f, jumpHeight * 0.8f);
 	private Vector2 NONE = Vector2.zero;
-
-	
 	float speed = 5.0f;
 	public Rigidbody2D _rigidBody;
 	private bool grounded = false;
@@ -22,11 +20,10 @@ public class RevenControler : MonoBehaviour {
 	private Transform rightWallCheck;
 	private Transform leftWallCheck;
 	private Transform _transform;
-
+	const float halfPlayerWidth = 0.16f;
 	private int foreground;
 
-
-	public virtual void Awake()
+	public virtual void Awake ()
 	{
 		groundCheck = transform.Find ("GroundCheck");
 		rightWallCheck = transform.Find ("RightWallCheck");
@@ -36,83 +33,76 @@ public class RevenControler : MonoBehaviour {
 
 	}
 
-	void Update () {
+	void Update ()
+	{
 
-		Vector2 jumpVector = getJumpVector ();
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+		Vector2 jumpVector = GetJumpVector ();
 
-		 float y = _rigidBody.velocity.y;
-		_rigidBody.velocity = new Vector2(moveHorizontal * speed, y);
+		MoveHorizontal ();
 
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
+		if (Input.GetKeyDown (KeyCode.Space)) {
 
-			print(jumpVector);
-			if ( jumpVector.Equals (FIRST) || jumpVector.Equals (SECOND) )
-			{
+			print (jumpVector);
+			if (jumpVector.Equals (FIRST) || jumpVector.Equals (SECOND)) {
 				jumps ++;
 			}
 			_rigidBody.velocity = Vector2.zero;
-			_rigidBody.AddForce( jumpVector , ForceMode2D.Impulse);
+			_rigidBody.AddForce (jumpVector, ForceMode2D.Impulse);
 		}
 
 	}
 
-	private bool isGrounded ()
+	void MoveHorizontal ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float y = _rigidBody.velocity.y;
+		Vector2 targetVelocity = new Vector2 (moveHorizontal * speed, y);
+		_rigidBody.velocity = Vector2.Lerp (_rigidBody.velocity, targetVelocity, Time.deltaTime);
+	
+	}
+
+	private bool IsGrounded ()
 	{
 		return _rigidBody.velocity.y == 0 && 
 			(Physics2D.Linecast (_transform.position, groundCheck.position, foreground)
-			||Physics2D.Linecast (new Vector2(_transform.position.x - 0.16f,_transform.position.y),
-			                      new Vector2(groundCheck.position.x - .16f,groundCheck.position.y), foreground)
-			||Physics2D.Linecast (new Vector2(_transform.position.x + 0.16f,_transform.position.y), 
-			                      new Vector2(groundCheck.position.x + .16f,groundCheck.position.y), foreground));
+			|| Physics2D.Linecast (new Vector2 (_transform.position.x - halfPlayerWidth, _transform.position.y),
+			                      new Vector2 (groundCheck.position.x - halfPlayerWidth, groundCheck.position.y), foreground)
+			|| Physics2D.Linecast (new Vector2 (_transform.position.x + halfPlayerWidth, _transform.position.y), 
+			                      new Vector2 (groundCheck.position.x + halfPlayerWidth, groundCheck.position.y), foreground));
 	}
 
-	private bool isOnRightWall ()
+	private bool IsOnRightWall ()
 	{
 		return _rigidBody.velocity.x == 0 && 
 			(Physics2D.Linecast (_transform.position, rightWallCheck.position, foreground)
-			 ||Physics2D.Linecast (new Vector2(_transform.position.x - 0.16f,_transform.position.y), 
-			                      new Vector2(rightWallCheck.position.x - .16f,rightWallCheck.position.y), foreground)
-			 ||Physics2D.Linecast (new Vector2(_transform.position.x + 0.16f,_transform.position.y), 
-			                      new Vector2(rightWallCheck.position.x + .16f,rightWallCheck.position.y), foreground));
+			|| Physics2D.Linecast (new Vector2 (_transform.position.x - halfPlayerWidth, _transform.position.y), 
+			                      new Vector2 (rightWallCheck.position.x - halfPlayerWidth, rightWallCheck.position.y), foreground)
+			|| Physics2D.Linecast (new Vector2 (_transform.position.x + halfPlayerWidth, _transform.position.y), 
+			                      new Vector2 (rightWallCheck.position.x + halfPlayerWidth, rightWallCheck.position.y), foreground));
 	}
 
-	private bool isOnLeftWall ()
+	private bool IsOnLeftWall ()
 	{
 		return _rigidBody.velocity.x == 0 && 
 			(Physics2D.Linecast (_transform.position, leftWallCheck.position, foreground)
-			 ||Physics2D.Linecast (new Vector2(_transform.position.x - 0.16f,_transform.position.y), 
-			                      new Vector2(leftWallCheck.position.x - .16f,leftWallCheck.position.y), foreground)
-			 ||Physics2D.Linecast (new Vector2(_transform.position.x + 0.16f,_transform.position.y), 
-			                      new Vector2(leftWallCheck.position.x + .16f,leftWallCheck.position.y), foreground));
+			|| Physics2D.Linecast (new Vector2 (_transform.position.x - halfPlayerWidth, _transform.position.y), 
+			                      new Vector2 (leftWallCheck.position.x - halfPlayerWidth, leftWallCheck.position.y), foreground)
+			|| Physics2D.Linecast (new Vector2 (_transform.position.x + halfPlayerWidth, _transform.position.y), 
+			                      new Vector2 (leftWallCheck.position.x + halfPlayerWidth, leftWallCheck.position.y), foreground));
 	}
 
-	private Vector2 getJumpVector ()
+	private Vector2 GetJumpVector ()
 	{
-		if (isGrounded ()) 
-		{
+		if (IsGrounded ()) {
 			jumps = 0;
 			return FIRST;
-		} 
-		else if (isOnRightWall ()) 
-		{
-			print("WALL_LEFT" + WALL_RIGHT);
+		} else if (IsOnRightWall ()) {
 			return WALL_LEFT;
-		} 
-		else if (isOnLeftWall ()) 
-		{
-			print("WALL_RIGHT" + WALL_LEFT);
+		} else if (IsOnLeftWall ()) {
 			return WALL_RIGHT;
-		} 
-		else if (jumps < maxJumps) 
-		{
-			print("SECOND" + SECOND);
+		} else if (jumps < maxJumps) {
 			return SECOND;
-		} 
-		else 
-		{
-			print("NONE" + NONE);
+		} else {
 			return NONE;
 		}
 	}
